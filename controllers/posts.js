30dda2d73eefb870d03e0d7525a419d9416f292f
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-29 14:40:30 +0200

    feat: content negotiation for the index route

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,18 +1,28 @@
 const postsList = require('../db/posts.json');
 
 const index = (req, res) => {
-    let html = ` <ul>
-                    ${postsList.map(p => `
-                    <li>
-                        <h2>${p.title}</h2>
-                        <p>${p.content}</p>
-                        <img width="250" src="/imgs/posts/${p.image}">
-                        <br>${p.tags.map(t => `<small>${t}</small>`).join(' - ')}
-                    `
-            ).join('')}
-    `
-    res.send(html);
+    res.format({
+        html: () => {
+            let html = ` <ul>
+                            ${postsList.map(p => `
+                            <li>
+                                <h2>${p.title}</h2>
+                                <p>${p.content}</p>
+                                <img width="250" src="/imgs/posts/${p.image}">
+                                <br>${p.tags.map(t => `<small>${t}</small>`).join(' - ')}
+                            `
+                    ).join('')}
+            `
+            res.send(html);
+        },
+        json: () => {
+            res.json(postsList);
+        }
+    })
 }
+
+
+
 
 module.exports = {
     index,
