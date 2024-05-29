2809207730cb5189674cdfb5de34ebda86de285b
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-29 16:21:03 +0200

    feat: proper token error

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const users = require('../db/users.json');
 require('dotenv').config();
 
-const generateToken = user => jwt.sign(user, process.env.JWT_SECRET, {expiresIn: "30m"});
+const generateToken = user => jwt.sign(user, process.env.JWT_SECRET, {expiresIn: "1m"});
 
 const login = (req, res) => {
     const {username,password} = req.body;
@@ -19,11 +19,36 @@ const login = (req, res) => {
 
 const authCheck = (req, res, next) => {
     const {authorization} = req.headers
+    if(!authorization){
+        return res.status(400).send('Error: No Authorization provided');
+    }
     const token = authorization.split(" ")[1];
     
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if(err){
-            return res.status(400).send('Token Error');
+            res.format({
+                json: () => {
+                    let errorObj = {
+                        error: true,
+                        status: 400,
+                        message: ""
+                    }
+                    switch(err.message){
+                        case "invalid token":
+                            errorObj.message = `The token is invalid, login to generate a proper token at ${req.baseUrl}/login`;
+                            return res.status(400).json(errorObj);
+                        case "jwt expired":
+                            errorObj.message = `The token expired at ${err.expiredAt}`;
+                            return res.status(400).json(errorObj);
+                    }
+                },
+                html: () => {
+                    if(err.message === "invalid token"){
+                        return res.status(400).send(`The token is invalid, login to generate a proper token at ${req.baseUrl}/login`);
+                    }
+                    return res.status(400).send(`The token expired at ${err.expiredAt}`);
+                }
+            })
         }
         next();
     })
--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -1,3 +1,13 @@
 module.exports = (req, res) => {
-    res.status(404).send('Page not found, sorry!');
+    res.format({
+        html: () => {
+            res.status(404).send('Page not found, sorry!');
+        },
+        json: () => {
+            res.status(404).json({
+                error: true,
+                description: `The page: ${req.url} has not been found`,
+            })
+        }
+    })
 }
